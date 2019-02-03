f276d255425032384041a82356b3cfbb99ea541e
Adelon Zeta <adelonzeta@Adelons-MacBook-Air.local>
2019-02-02 21:08:57 -0800

    implement sanitize function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+import replace from 'lodash/replace'
 
-export default function App() {
+export default () => {
+  const [state, dispatch] = useState('Paste your HTML here...')
+
+  function sanitize(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    let newHTML = ''
+    const HTML = e.target.value
+    const stylePattern = /<img alt=""/gm
+    const endingPattern = />/gm
+    const addStyle = '<img alt="" style="display:block;"'
+    const newLine = '>\n'
+    newHTML = replace(HTML, stylePattern, addStyle)
+    newHTML = replace(newHTML, endingPattern, newLine)
+    dispatch(newHTML)
+  }
+
   return (
     <div>
-      test
+      <textarea
+        id="textarea"
+        cols={30} 
+        rows={10} 
+        onChange={sanitize}
+        value={state}>
+      </textarea>
     </div>
   )
 }
